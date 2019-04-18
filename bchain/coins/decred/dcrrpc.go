8085f43bca23dc1dab71abef5de3f38eeb43eaec
Bode <Bode>
2019-04-18 13:06:53 +0100

    decred port

---
+++ bchain/coins/decred/dcrrpc.go
@@ -0,0 +1,72 @@
+package dcr
+import (
+	"blockbook/bchain"
+	"blockbook/bchain/coins/btc"
+	"encoding/json"
+
+	"github.com/golang/glog"
+)
+
+// DecredRPC is an interface to JSON-RPC dcrd service.
+type DecredRPC struct {
+	*btc.BitcoinRPC
+}
+
+// NewDecredRPC returns new DecredRPC instance.
+func NewDecredRPC(config json.RawMessage, pushHandler func(bchain.NotificationType)) (bchain.BlockChain, error) {
+	b, err := btc.NewBitcoinRPC(config, pushHandler)
+	if err != nil {
+		return nil, err
+	}
+
+	s := &DecredRPC{
+		b.(*btc.BitcoinRPC),
+	}
+	s.RPCMarshaler = btc.JSONMarshalerV1{}
+	s.ChainConfig.SupportsEstimateSmartFee = false
+
+	return s, nil
+}
+
+// Initialize initializes DogecoinRPC instance.
+func (b *DecredRPC) Initialize() error {
+	chainName, err := b.GetChainInfoAndInitializeMempool(b)
+	if err != nil {
+		return err
+	}
+
+	glog.Info("Chain name ", chainName)
+	params := GetChainParams(chainName)
+
+	// always create parser
+	b.Parser = NewDecredParser(params, b.ChainConfig)
+
+	// parameters for getInfo request
+	if params.Net == MainnetMagic {
+		b.Testnet = false
+		b.Network = "livenet"
+	} else {
+		b.Testnet = true
+		b.Network = "testnet"
+	}
+
+	glog.Info("rpc: block chain ", params.Name)
+
+	return nil
+}
+
+// GetBlock returns block with given hash.
+func (b *DecredRPC) GetBlock(hash string, height uint32) (*bchain.Block, error) {
+	var err error
+	if hash == "" {
+		hash, err = b.GetBlockHash(height)
+		if err != nil {
+			return nil, err
+		}
+	}
+	if !b.ParseBlocks {
+		return b.GetBlockFull(hash)
+	}
+	return b.GetBlockWithoutHeader(hash, height)
+}
+
